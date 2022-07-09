64704e40605cd2db8f3db4f4f36ffbdec776a2cf
Kaustubh Singh <97877838+KaustubhSingh1403@users.noreply.github.com>
2022-07-10 01:46:02 +0530

    First iteration of changes in the file

--- Dictionary.h
+++ Dictionary.h
@@ -19,14 +19,18 @@ class Dictionary {
 
     umap dictionary;
     vect closed_form_words;
+    vect palindrome_words;
 
     public:
-    Dictionary(string fileName); // contructor that makes an unordered map dictionary of the given filename
-    umap makeDictionary(string fileName); // called int Dictionary constructor to make an unordered_map dictionary
-    umap getDictionary(); // returns member dictionary
-    vect closed_form(); // generates a vector comprising of all closed form compound words
-    vect getClosedFormWords(); // returns member closed_form_words
-    bool word_present(string word); // checks for the presence of a word in the dictionary
+    Dictionary(string fileName);            // contructor that makes an unordered map dictionary of the given filename
+    umap makeDictionary(string fileName);   // called int Dictionary constructor to make an unordered_map dictionary
+    umap getDictionary();                   // returns member dictionary
+    vect closed_form();                     // generates a vector comprising of all closed form compound words
+    vect getClosedFormWords();              // returns member closed_form_words
+    vect palindrome();                      // generates a vector comprisinf of all palindrome words
+    vect getPalindromeWords();              // returns member palindrome_words
+    bool word_present(string word);         // checks for the presence of a word in the dictionary
+    bool is_palindrome(string word);        // checks if word is palindrome
 };
 
 #endif
